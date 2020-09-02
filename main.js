96b95a3c2295b50f993829837960dbb361159118
anitsingh367 <anitsingh367@gmail.com>
2020-09-02 18:32:49 +0530

    Removed Tabs and linked sections, added navbar responsiveness

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-document.getElementById("defaultOpen").click();
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "flex";
-  evt.currentTarget.className += " active";
-}
 
 var glide = new Glide(".glide", {
   type: "carousel",
@@ -38,7 +18,7 @@ var glide = new Glide(".glide", {
 
 glide.mount();
 
-var glide1 = new Glide(".glide1",{
+var glide1 = new Glide(".glide1", {
   type: "carousel",
   startAt: 0,
   gap: 45,
@@ -55,7 +35,7 @@ var glide1 = new Glide(".glide1",{
 });
 glide1.mount();
 
-var glide2 = new Glide(".glide2",{
+var glide2 = new Glide(".glide2", {
   type: "carousel",
   startAt: 0,
   gap: 45,
@@ -71,3 +51,12 @@ var glide2 = new Glide(".glide2",{
   },
 });
 glide2.mount();
+
+let mainNav = document.getElementById("js-menu");
+let navbar = document.getElementById("navbar")
+let navBarToggle = document.getElementById("js-navbar-toggle");
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+  navbar.classList.toggle("active2");
+  
+});
